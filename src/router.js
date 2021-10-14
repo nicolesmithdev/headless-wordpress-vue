d340f0bcd6ccbc9148646e6a4fe91bd533ef4c9e
nicolesmithweb <nicolesmithweb@me.com>
2021-10-14 09:39:07 -0400

    Scroll to top when navigating to a new route

--- src/router.js
+++ src/router.js
@@ -9,33 +9,33 @@ Vue.use(VueRouter);
 const router = new VueRouter({
     mode: 'history',
     routes: [
-      {
-        path: '/',
-        name: 'home',
-        component: Home,
-      },
-      {
-        path: '/:slug',
-        name: 'page',
-        component: SinglePage
-      },
-      {
-        path: '/:catchall(.*)',
-        name: 'catchall',
-        component: Home
-      }
-    ]
-});
-
-router.beforeEach(function(to, from, next) {
-  if (from.name) {
-    document.body.classList.remove(from.name);
-  }
-  if (to.name) {
-    document.body.classList.add(to.name);
-  }
-
-  next();
+        {
+            path: '/',
+            name: 'home',
+            component: Home,
+        },
+        {
+            path: '/:slug',
+            name: 'page',
+            component: SinglePage,
+        },
+        {
+            path: '/:catchall(.*)',
+            name: 'catchall',
+            component: Home,
+        },
+    ],
+    scrollBehavior(to, from, savedPosition) {
+        window.history.scrollRestoration = 'manual';
+        if (to.hash) {
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve({ selector: to.hash, behavior: 'smooth' });
+                }, 500);
+            });
+        }
+        return savedPosition ? savedPosition : { x: 0, y: 0 };
+    },
 });
 
 export default router;

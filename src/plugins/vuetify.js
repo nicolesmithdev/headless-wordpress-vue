08fc440b9d8afd3cfdcc88afb82d3b19c75c9080
nicolesmithweb <nicolesmithweb@me.com>
2021-10-20 13:34:44 -0400

    Define Vuetify breakpoints

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,13 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    breakpoint: {
+        thresholds: {
+            xs: 576,
+            sm: 768,
+            md: 992,
+            lg: 1200,
+        },
+        scrollBarWidth: 16,
+    },
 });

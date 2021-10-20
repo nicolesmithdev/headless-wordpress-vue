6d010f7c94d4a90df8f253e0c10a82971315d3f1
nicolesmithweb <nicolesmithweb@me.com>
2021-10-20 13:34:30 -0400

    Improve search

--- src/components/layout/sidebar/Search.vue
+++ src/components/layout/sidebar/Search.vue
@@ -1,28 +1,41 @@
 <template>
     <form>
-        <input type="search" name="search" v-model="searchQuery" placeholder="Search this website" @input="onSearch" />
+        <input
+            type="search"
+            name="search"
+            v-model="searchQuery"
+            placeholder="Search this website"
+            @input="onSearch"
+        />
     </form>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            'searchQuery': ''
-        };
+    computed: {
+        searchQuery: {
+            get() {
+                return this.$store.getters.PROP('searchQuery');
+            },
+            set(value) {
+                this.$store.dispatch('PROP', {
+                    prop: 'searchQuery',
+                    value: value,
+                });
+            },
+        },
     },
     methods: {
         onSearch() {
-            this.$store.dispatch('SEARCH_TERM', this.searchQuery);
-            this.$router.replace('/?s='+this.searchQuery);
-        }
+            this.$router.replace('/?s=' + this.searchQuery);
+        },
     },
     watch: {
-        '$route' (to, from) {
-            if ( from.query.s && to.fullPath == "/") {
+        $route(to, from) {
+            if (from.query.s && to.fullPath == '/') {
                 this.searchQuery = '';
             }
-        }
-    }
-}
+        },
+    },
+};
 </script>

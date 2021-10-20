8aca649c5cfe1b8d95e6aa348f7d0e36bf6b1a26
nicolesmithweb <nicolesmithweb@me.com>
2021-10-20 13:59:47 -0400

    Install Moment.js

--- src/components/SinglePage.vue
+++ src/components/SinglePage.vue
@@ -1,7 +1,12 @@
 <template>
     <div :class="pageClasses">
-        <BaseSpinner v-if="isLoading"/>
+        <BaseSpinner v-if="isLoading" />
         <template v-else>
+            <p
+                v-if="page.type == 'post'"
+                class="entry-meta"
+                v-html="publishedDate"
+            />
             <h1 class="entry-title" v-html="page.title.rendered"></h1>
             <div v-html="page.content.rendered" class="entry-content"></div>
         </template>
@@ -10,32 +15,36 @@
 
 <script>
 import { mapGetters } from 'vuex';
-
+import moment from 'moment';
 export default {
     data() {
         return {
-            isLoading: false
+            isLoading: false,
         };
     },
     computed: {
         ...mapGetters(['page']),
         pageClasses() {
             return 'post-' + this.page.id + ' page entry';
-        }
+        },
+        publishedDate() {
+            return moment(this.page.date).format('MMMM d, YYYY');
+        },
     },
     methods: {
         async loadPage() {
             this.isLoading = true;
 
             try {
-                await this.$store.dispatch('LOAD_PAGE', { route: this.$route.params.slug });
-            }
-            catch(error) {
+                await this.$store.dispatch('LOAD_PAGE', {
+                    route: this.$route.params.slug,
+                });
+            } catch (error) {
                 console.log('loadPage error', error);
             }
-            
+
             this.isLoading = false;
-        }
+        },
     },
     created() {
         this.loadPage();
@@ -43,7 +52,7 @@ export default {
     watch: {
         $route() {
             this.loadPage();
-        }
-    }
-}
+        },
+    },
+};
 </script>
--- src/components/pages/home/SinglePost.vue
+++ src/components/pages/home/SinglePost.vue
@@ -1,10 +1,11 @@
 <template>
     <div :class="postClasses">
-        <p class="entry-meta">
-            {{ publishedDate }}
-        </p>
+        <p class="entry-meta" v-html="publishedDate" />
         <h2 class="entry-title">
-            <router-link :to="{ path: `/` + post.slug, params: {} }" v-html="post.title.rendered"></router-link>
+            <router-link
+                :to="{ path: `/` + post.slug, params: {} }"
+                v-html="post.title.rendered"
+            ></router-link>
         </h2>
         <router-link :to="`/` + post.slug" v-if="image">
             <v-img
@@ -20,12 +21,12 @@
 </template>
 
 <script>
+import moment from 'moment';
 export default {
-    // props: ['id', 'slug', 'title', 'excerpt', 'date', 'featuredImage', 'featuredImageUrls'],
     props: ['post'],
     data() {
         return {
-            image: ''
+            image: '',
         };
     },
     created() {
@@ -33,34 +34,38 @@ export default {
     },
     methods: {
         featuredImg() {
-            if (this.post.featured_image_src && this.post.featured_image_urls.large) {
+            if (
+                this.post.featured_image_src &&
+                this.post.featured_image_urls.large
+            ) {
                 this.image = this.post.featured_image_urls.large[0];
             } else {
-                this.$http.get('wp/v2/media?parent=' + this.post.id + '&type=image')
-                    .then((response) => {
-                        this.image = response.data[0].source_url;
-                    }, error => {
-                        console.log(error);
-                    });
+                this.$http
+                    .get('wp/v2/media?parent=' + this.post.id + '&type=image')
+                    .then(
+                        (response) => {
+                            this.image = response.data[0].source_url;
+                        },
+                        (error) => {
+                            console.log(error);
+                        }
+                    );
             }
-        }
+        },
     },
     computed: {
         lazySrc() {
-            return this.post.featured_image_urls && this.post.featured_image_urls.thumbnail[0] ? this.post.featured_image_urls.thumbnail[0] : this.image;
+            return this.post.featured_image_urls &&
+                this.post.featured_image_urls.thumbnail[0]
+                ? this.post.featured_image_urls.thumbnail[0]
+                : this.image;
         },
         postClasses() {
             return 'post-' + this.post.id + ' post entry';
         },
         publishedDate() {
-            const d = new Date(this.post.date);
-            const months = ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-            const month = months[d.getMonth()];
-            const date = d.getDate();
-            const year = d.getFullYear();
-            
-            return month + ' ' + date + ', ' + year;
-        }
-    } 
-}
+            return moment(this.post.date).format('MMMM d, YYYY');
+        },
+    },
+};
 </script>

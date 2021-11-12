1483f60d9f9c22fa55e586e25d3517a36fdad74c
nicolesmithweb <nicolesmithweb@me.com>
2021-11-12 17:23:03 -0500

    Cleanup Gravity Forms API

--- admin/index.js
+++ admin/index.js
@@ -9,9 +9,20 @@ const axios = require('axios');
 const multer = require('multer');
 const upload = multer();
 
+// for parsing application/json
+app.use(express.json());
+
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// for parsing multipart/form-data
+app.use(upload.array());
+app.use(express.static('public'));
+
 /**
  * Define parameters
  */
+const baseUrl = 'http://localhost:8888/wp-json/gf/v2';
 const clientKey = process.env.CLIENT_KEY;
 const clientSecret = process.env.CLIENT_SECRET;
 const signatureMethod = 'HMAC-SHA1';
@@ -46,52 +57,50 @@ for (let k in ordered) {
 }
 
 /**
- * Create OAuth signature
+ * Generate OAuth signature
  */
-const method = 'POST';
-const baseUrl = 'http://localhost:8888/wp-json/gf/v2/entries';
-const encodedUrl = encodeURIComponent(baseUrl);
-encodedParameters = encodeURIComponent(encodedParameters);
-const signatureBaseString = `${method}&${encodedUrl}&${encodedParameters}`;
-const signingKey = `${clientSecret}&`;
-const oauth_signature = crypto
-    .createHmac('sha1', signingKey)
-    .update(signatureBaseString)
-    .digest()
-    .toString('base64');
-
-const querystring = {
-    oauth_consumer_key: clientKey,
-    oauth_signature_method: signatureMethod,
-    oauth_timestamp: timestamp,
-    oauth_nonce: nonce,
-    oauth_version: '1.0',
-    oauth_signature: encodeURIComponent(oauth_signature),
-};
-
-// for parsing application/json
-app.use(express.json());
-
-// for parsing application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-
-// for parsing multipart/form-data
-app.use(upload.array());
-app.use(express.static('public'));
+function oauth_signature(method, baseUrl, encodedParams, clientSecret) {
+    const encodedUrl = encodeURIComponent(baseUrl);
+    const encodedParameters = encodeURIComponent(encodedParams);
+    const signatureBaseString = `${method}&${encodedUrl}&${encodedParameters}`;
+    const signingKey = `${clientSecret}&`;
+    return (oauth_signature = crypto
+        .createHmac('sha1', signingKey)
+        .update(signatureBaseString)
+        .digest()
+        .toString('base64'));
+}
 
 /**
  * Post to Gravity Forms API
  */
 app.post('/submit', (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
-    axios({
-        method: 'POST',
-        url: `${baseUrl}?${qs.stringify(querystring)}`,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        data: JSON.stringify(req.body),
-    }).then((response) => res.send(response.data));
+    let url = `${baseUrl}/entries?${qs.stringify({
+        oauth_consumer_key: clientKey,
+        oauth_signature_method: signatureMethod,
+        oauth_timestamp: timestamp,
+        oauth_nonce: nonce,
+        oauth_version: '1.0',
+        oauth_signature: encodeURIComponent(
+            oauth_signature(
+                'POST',
+                `${baseUrl}/entries`,
+                encodedParameters,
+                clientSecret
+            )
+        ),
+    })}`;
+    let data = JSON.stringify(req.body);
+    let headers = {
+        'Content-Type': 'application/json',
+    };
+    axios
+        .post(url, data, { headers: headers })
+        .then((response) => res.send(response.data))
+        .catch((error) => {
+            res.send(error);
+        });
 });
 
 app.listen(port, () => {
